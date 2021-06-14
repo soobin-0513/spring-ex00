fcb8fc0577a135ad2c3a77653cb9345d33cfa81a
soobin <rxb0513@gmail.com>
2021-06-14 17:05:13 +0900

    .

--- src/main/java/lombok/test/Desk.java
+++ src/main/java/lombok/test/Desk.java
@@ -1,5 +1,14 @@
 package lombok.test;
 
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+
+@ToString
+
+@RequiredArgsConstructor
 public class Desk {
 
+	private final double price;
+	private final String model;
+	private String name;
 }
