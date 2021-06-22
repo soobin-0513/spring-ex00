9fd81e115090860b231dbda3c9e16e6943ab3100
soobin <rxb0513@gmail.com>
2021-06-22 10:22:10 +0900

    06.22_ spring test3

---
+++ src/main/java/core/test3/App.java
@@ -0,0 +1,24 @@
+package core.test3;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+
+	public static void main(String[] args) {
+		String path = "core/test3/core-test3.xml";
+		ApplicationContext context = new ClassPathXmlApplicationContext(path);
+
+		Hero hero = context.getBean("hero", Hero.class);
+
+		System.out.println(hero);
+		System.out.println(hero.getSword());
+
+		System.out.println("프로그램 종료.");
+		
+		
+		//Object o1 = context.getBean("hero");
+		//System.out.println(o1);
+		
+	}
+}	
