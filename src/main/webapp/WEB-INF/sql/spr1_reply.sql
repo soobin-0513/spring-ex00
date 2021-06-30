86c5a6bb8356ec7491ebf2349f49eada5a25be50
soobin <rxb0513@gmail.com>
2021-06-30 16:20:59 +0900

    sql

---
+++ src/main/webapp/WEB-INF/sql/spr1_reply.sql
@@ -0,0 +1,21 @@
+USE spr1;
+
+CREATE TABLE tbl_reply(
+	rno INT PRIMARY KEY AUTO_INCREMENT,
+    bno INT NOT NULL,
+    reply VARCHAR(512) NOT NULL,
+    replyer VARCHAR(50) NOT NULL,
+    replyDate TIMESTAMP DEFAULT NOW(),
+	updateDate TIMESTAMP DEFAULT NOW(),
+    FOREIGN KEY (bno) REFERENCES tbl_board(bno)
+);
+
+SELECT * FROM tbl_board
+ORDER BY bno DESC;
+SELECT * FROM tbl_reply
+ORDER BY bno DESC;
+
+-- 댓글이 있는 게시물 조회 
+SELECT
+DISTINCT(b.bno)
+FROM tbl_board b JOIN tbl_reply r ON b.bno = r.bno;
